e2b7babbadc4fa7939ae1939b862b48df732b86d
Ngô Đức Thọ <45930156+3ei3isaki@users.noreply.github.com>
2020-11-07 12:19:14 +0700

    divide admin UI, init admin test feature

---
+++ resources/views/admin/components/script.blade.php
@@ -0,0 +1,73 @@
+{{-- Bottom of body tag --}}
+
+
+<!-- jQuery -->
+<script src="plugins/jquery/jquery.min.js"></script>
+<!-- jQuery UI 1.11.4 -->
+<script src="plugins/jquery-ui/jquery-ui.min.js"></script>
+<!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
+<script>
+    $.widget.bridge('uibutton', $.ui.button)
+</script>
+<!-- Bootstrap 4 -->
+<script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- ChartJS -->
+<script src="plugins/chart.js/Chart.min.js"></script>
+<!-- Sparkline -->
+<script src="plugins/sparklines/sparkline.js"></script>
+<!-- JQVMap -->
+<script src="plugins/jqvmap/jquery.vmap.min.js"></script>
+<script src="plugins/jqvmap/maps/jquery.vmap.usa.js"></script>
+<!-- jQuery Knob Chart -->
+<script src="plugins/jquery-knob/jquery.knob.min.js"></script>
+<!-- daterangepicker -->
+<script src="plugins/moment/moment.min.js"></script>
+<script src="plugins/daterangepicker/daterangepicker.js"></script>
+<!-- Tempusdominus Bootstrap 4 -->
+<script src="plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js"></script>
+<!-- Summernote -->
+<script src="plugins/summernote/summernote-bs4.min.js"></script>
+<!-- overlayScrollbars -->
+<script src="plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
+<!-- AdminLTE App -->
+<script src="dist/js/adminlte.js"></script>
+<!-- AdminLTE dashboard demo (This is only for demo purposes) -->
+<script src="dist/js/pages/dashboard.js"></script>
+<!-- AdminLTE for demo purposes -->
+<script src="dist/js/demo.js"></script>
+<script>
+    $('#calendar').datepicker({
+});
+
+!function ($) {
+    $(document).on("click","ul.nav li.parent > a > span.icon", function(){
+        $(this).find('em:first').toggleClass("glyphicon-minus");
+    });
+    $(".sidebar span.icon").find('em:first').addClass("glyphicon-plus");
+}(window.jQuery);
+
+$(window).on('resize', function () {
+  if ($(window).width() > 768) $('#sidebar-collapse').collapse('show')
+})
+$(window).on('resize', function () {
+  if ($(window).width() <= 767) $('#sidebar-collapse').collapse('hide')
+})
+// Chang Image add product
+function changeImg(input){
+    //Nếu như tồn thuộc tính file, đồng nghĩa người dùng đã chọn file mới
+    if(input.files && input.files[0]){
+        var reader = new FileReader();
+        //Sự kiện file đã được load vào website
+        reader.onload = function(e){
+            //Thay đổi đường dẫn ảnh
+            $('#avatar').attr('src',e.target.result);
+        }
+        reader.readAsDataURL(input.files[0]);
+    }
+}
+$(document).ready(function() {
+    $('#avatar').click(function(){
+        $('#img').click();
+    });
+});
+</script>
--- routes/web.php
+++ routes/web.php
@@ -18,24 +18,24 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', 'App\Http\Controllers\Client\HomeController@index')->name('home');
 
 //Course
-Route::get('/khoa-hoc','App\Http\Controllers\Client\CourseController@list')->name('courseList');
-Route::get('/khoa-hoc/danh-muc','App\Http\Controllers\Client\CourseController@index')->name('course');
-Route::get('/khoa-hoc/danh-muc/chi-tiet','App\Http\Controllers\Client\CourseController@detail')->name('courseDetail');
+Route::get('/khoa-hoc', 'App\Http\Controllers\Client\CourseController@list')->name('courseList');
+Route::get('/khoa-hoc/danh-muc', 'App\Http\Controllers\Client\CourseController@index')->name('course');
+Route::get('/khoa-hoc/danh-muc/chi-tiet', 'App\Http\Controllers\Client\CourseController@detail')->name('courseDetail');
 
 //Contact
-Route::get('/lien-he','App\Http\Controllers\Client\ContactController@index')->name('contact');
+Route::get('/lien-he', 'App\Http\Controllers\Client\ContactController@index')->name('contact');
 
 //News
-Route::get('/tin-tuc','App\Http\Controllers\Client\NewsController@index')->name('news');
-Route::get('/tin-tuc/chi-tiet','App\Http\Controllers\Client\NewsController@detail')->name('newsDetail');
+Route::get('/tin-tuc', 'App\Http\Controllers\Client\NewsController@index')->name('news');
+Route::get('/tin-tuc/chi-tiet', 'App\Http\Controllers\Client\NewsController@detail')->name('newsDetail');
 
 //test
-Route::get('/test','App\Http\Controllers\client\ExamController@index')->name('test');
+Route::get('/test', 'App\Http\Controllers\client\ExamController@index')->name('test');
 //login
-Route::get('/dang-nhap','App\Http\Controllers\Client\LoginController@index')->name('login');
+Route::get('/dang-nhap', 'App\Http\Controllers\Client\LoginController@index')->name('login');
 
 //Register
-Route::group(['namespace'=>'App\Http\Controllers'],function(){
+Route::group(['namespace' => 'App\Http\Controllers'], function () {
     Route::group(['prefix' => 'register'], function () {
         Route::get('/', 'RegisterController@index')->name('registerView');
         Route::post('/', 'RegisterController@register')->name('register');
@@ -43,44 +43,41 @@ Route::group(['namespace'=>'App\Http\Controllers'],function(){
 });
 
 //Admin
-Route::group(['namespace'=>'App\Http\Controllers\Admin'],function(){
-    Route::group(['prefix'=>'login','middleware'=>'CheckLogedIn'],function(){
-        Route::get('/','LoginController@getLogin')->name('login');
-        Route::post('/','LoginController@postLogin');
+Route::group(['namespace' => 'App\Http\Controllers\Admin'], function () {
+    Route::group(['prefix' => 'login', 'middleware' => 'CheckLogedIn'], function () {
+        Route::get('/', 'LoginController@getLogin')->name('login');
+        Route::post('/', 'LoginController@postLogin');
     });
 
-    Route::get('logout','HomeController@getLogout')->name('logout');
+    Route::get('logout', 'HomeController@getLogout')->name('logout');
 
-    Route::group(['prefix'=>'admin','middleware'=>'CheckLogedOut','middleware'=>'App\Http\Middleware\RequireAuth'],function(){
-        Route::get('/','HomeController@getHome');
+    Route::group(['prefix' => 'admin', 'middleware' => 'CheckLogedOut', 'middleware' => 'App\Http\Middleware\RequireAuth'], function () {
+        Route::get('/', 'HomeController@getHome');
 
-        Route::group(['prefix'=>'user'], function(){
-            Route::get('/','UserController@getUser');
+        Route::group(['prefix' => 'user'], function () {
+            Route::get('/', 'UserController@getUser');
 
-            Route::get('add','UserController@getAddUser');
-            Route::post('add','UserController@postAddUser');
+            Route::get('add', 'UserController@getAddUser');
+            Route::post('add', 'UserController@postAddUser');
 
-            Route::get('edit/{id}','UserController@getEditUser');
-            Route::post('edit/{id}','UserController@postEditUser');
+            Route::get('edit/{id}', 'UserController@getEditUser');
+            Route::post('edit/{id}', 'UserController@postEditUser');
 
-            Route::get('delete/{id}','UserController@getDeleteUser');
-         });
+            Route::get('delete/{id}', 'UserController@getDeleteUser');
+        });
 
-         Route::group(['prefix'=>'banner'], function(){
-            Route::get('/','BannerController@getBanner');
+        Route::group(['prefix' => 'banner'], function () {
+            Route::get('/', 'BannerController@getBanner');
 
-            Route::get('add','BannerController@getAddBanner');
-            Route::post('add','BannerController@postAddBanner');
+            Route::get('add', 'BannerController@getAddBanner');
+            Route::post('add', 'BannerController@postAddBanner');
 
-            Route::get('edit/{id}','BannerController@getEditBanner');
-            Route::post('edit/{id}','BannerController@postEditBanner');
+            Route::get('edit/{id}', 'BannerController@getEditBanner');
+            Route::post('edit/{id}', 'BannerController@postEditBanner');
 
-            Route::get('delete/{id}','BannerController@getDeleteBanner');
-         });
+            Route::get('delete/{id}', 'BannerController@getDeleteBanner');
+        });
     });
-<<<<<<< HEAD
-});
-=======
 
     Route::group(['prefix' => 'news'], function () {
 
@@ -91,16 +88,15 @@ Route::group(['namespace'=>'App\Http\Controllers\Admin'],function(){
         Route::post('/add', 'NewController@postAdd')->name('adminNewPostAdd');
 
         Route::get('/edit/{id}', 'NewController@getEdit')
-             ->name('adminNewGetEdit')
-             ->where(['id' => '[0-9]+']);
+            ->name('adminNewGetEdit')
+            ->where(['id' => '[0-9]+']);
 
         Route::post('/edit/{id}', 'NewController@putEdit')
-             ->name('adminNewPutEdit')
-             ->where(['id' => '[0-9]+']);
+            ->name('adminNewPutEdit')
+            ->where(['id' => '[0-9]+']);
 
         Route::get('/delete/{id}', 'NewController@delete')
-             ->name('adminNewDelete')
-             ->where(['id' => '[0-9]+']);
-   });
+            ->name('adminNewDelete')
+            ->where(['id' => '[0-9]+']);
+    });
 });
->>>>>>> 5d346ffa82622703a501ef1a563754f293af0491
