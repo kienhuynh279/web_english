aa67d0b73d476e9da99f2fd315ffcef709b11bb9
kienhuynh279 <kienhuynh279@gmail.com>
2020-11-09 03:46:47 +0700

    Create component Input Form

--- resources/views/admin/page/news/inc/_form.blade.php
+++ resources/views/admin/page/news/inc/_form.blade.php
@@ -1,1 +1,5 @@
 <h1>Form Controll</h1>
+  <div class="form-row">
+        <x-input-form type="text" label="Tiêu đề" name="Title" classGroup="col-md" class="" placeholder="Tiêu đề"
+            :value="$blog->Title ?? ''" />
+    </div>
