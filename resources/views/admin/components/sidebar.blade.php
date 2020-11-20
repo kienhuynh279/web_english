b23b4842ff4ac7fb3b87ccc3bcc1f0ae69e22f7f
kienhuynh279 <kienhuynh279@gmail.com>
2020-11-20 23:29:33 +0700

    Fix side bar admin

--- resources/views/admin/components/sidebar.blade.php
+++ resources/views/admin/components/sidebar.blade.php
@@ -64,27 +64,13 @@
                 </li>
 
                 <li class="nav-item">
-                    <a href="javascript:void(0)" class="nav-link">
+                    <a href="{{ route('adminNew') }}" class="nav-link">
                         <i class="nav-icon fas fa-question"></i>
                         <p> 
                             Quản Lý Blogs
-                            <i class="right fas fa-angle-left"></i>
+                          
                         </p>
                     </a>
-                    <ul class="nav nav-treeview">
-                        <li class="nav-item">
-                            <a href="{{ route('adminBlogCate') }}" class="nav-link">
-                                <i class="far fa-circle nav-icon"></i>
-                                <p>Loại Bài Blogs</p>
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="{{ route('adminNew') }}" class="nav-link">
-                                <i class="far fa-circle nav-icon"></i>
-                                <p>Danh Sách Blogs</p>
-                            </a>
-                        </li>
-                    </ul>
                 </li>
 
 
@@ -113,27 +99,13 @@
                 </li>
 
                 <li class="nav-item">
-                    <a href="javascript:void(0)" class="nav-link">
+                    <a href="{{ route('adminMakeTest') }}" class="nav-link">
                         <i class="nav-icon fas fa-question"></i>
                         <p> 
                             Quản Lý Bài Kiểm Tra
-                            <i class="right fas fa-angle-left"></i>
+                         
                         </p>
                     </a>
-                    <ul class="nav nav-treeview">
-                        <li class="nav-item">
-                            <a href="{{ route('adminFormCate') }}" class="nav-link">
-                                <i class="far fa-circle nav-icon"></i>
-                                <p>Loại Bài Kiểm Tra</p>
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="{{ route('adminMakeTest') }}" class="nav-link">
-                                <i class="far fa-circle nav-icon"></i>
-                                <p>Danh Sách Bài Kiểm tra</p>
-                            </a>
-                        </li>
-                    </ul>
                 </li>
             </ul>
         </nav>
