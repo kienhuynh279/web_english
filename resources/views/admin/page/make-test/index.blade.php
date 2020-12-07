9a2021bbd9782d2e9001bfe60e16098eb73ad608
Ngô Đức Thọ <45930156+3ei3isaki@users.noreply.github.com>
2020-12-07 17:18:46 +0700

    Filter Admin Exam

--- resources/views/admin/page/make-test/index.blade.php
+++ resources/views/admin/page/make-test/index.blade.php
@@ -2,17 +2,36 @@
 @section('title','Danh sách bài kiểm tra')
 @section('main')
 <div class="card">
-    <div class="card-header">
-        <a href="{{ route('adminMakeTestGetAdd') }}" class="btn btn-sm btn-primary">Thêm bài kiểm tra</a>
+    <div class="card-header d-flex align-items-center" style="gap: 15px;">
+        <a href="{{ route('adminMakeTestGetAdd') }}" class="btn btn-primary">Thêm bài kiểm tra</a>
+        <form id="cate-form" action="{{ route('adminMakeTest') }}" method="get">
+            <div class="input-group">
+                <div class="input-group-prepend">
+                    <span class="input-group-text">Loại bài</span>
+                </div>
+                <select id="cate-input" class="form-control" name="cate">
+                    <option value="" aria-readonly="true">Tất cả</option>
+                    @isset($CategoryData)
+                    @foreach ($CategoryData as $item)
+                    <optgroup label="{{ $item->title }}">
+                        @foreach($item->child as $child_item)
+                        <option value="1{{ ($item->id > 9 ? $item->id : "0".$item->id).($child_item->id > 9 ? $child_item->id : "0".$child_item->id)}}" {{ $child_item->id == substr($CategoryData->selected ?? '', 3) ? 'selected' : '' }}>
+                            {{ $child_item->title }}</option>
+                        @endforeach
+                    </optgroup>
+                    @endforeach
+                    @endisset
+                </select>
+            </div>
+        </form>
     </div>
     <div class="card-body">
         <table id="dataTable" class="table table-striped table-bordered" style="width:100%">
             <thead>
                 <tr>
+                    <th>ID</th>
                     <th>Tên Bài Kiểm Tra</th>
-                    <th>Mô tả ngắn</th>
                     <th>Trạng thái</th>
-                    <th>Ngày đăng</th>
                     <th>
                         <button disabled class="btn btn-sm btn-secondary">
                             <i class="fas fa-edit"></i>
@@ -26,10 +45,9 @@
             <tbody>
                 @foreach ($forms as $form)
                 <tr>
+                    <th>{{ $form->id }}</th>
                     <th>{{ $form->title }}</th>
-                    <th>{!! $form->summary !!}</th>
-                    <th>{{ $form->status }}</th>
-                    <th>{{ $form->created_at }}</th>
+                    <td class="text-{{ $form['status'] == 1 ? "success" : "danger" }}">{{ $form['status'] == 1 ? "Hiện" : "Ẩn" }}</td>
                     <th>
                         <a href="{{ route('adminMakeTestGetEdit', $form->id) }}" class="btn btn-sm btn-primary">
                             <i class="fas fa-edit"></i>
@@ -57,9 +75,9 @@
         // searching: false,
         aaSorting: [],
         bInfo : false,
-        // columnDefs: [
-        //     { className: "text-nowrap text-center", "targets": [ -1 ] }
-        // ],
+        columnDefs: [
+            { className: "text-nowrap text-center", "targets": [ 0, 2 ] }
+        ],
         fixedColumns: {
             leftColumns: 0,
             rightColumns: 1
@@ -67,5 +85,15 @@
     });
 
     new ResizeSensor($("#dataTable"), () => table.draw());
+
+    // filter
+    let cateInput = document.getElementById('cate-input');
+    let cateForm = document.getElementById('cate-form');
+
+    cateInput.addEventListener('change', function() {
+        // cateForm.action = `${cateForm.attributes['data-base-url'].value}?cate=${this.value}`;
+        // console.log(cateForm.action);
+        cateForm.submit();
+    })
 </script>
 @stop
